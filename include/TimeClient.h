4dc4db064d4e63b3dcbfae4ace21f9a8907e3a1a
Andrea Mancini <mancioman2@gmail.com>
2024-03-22 20:44:17 +0100

    some comments for doxygen

--- include/TimeClient.h
+++ include/TimeClient.h
@@ -1,8 +1,37 @@
+/**
+ * @file TimeClient.h
+ * @brief Interface for time retrieval and NTP client setup.
+ *
+ * This header file provides functions for setting up a time client and retrieving
+ * the current time. It is designed to abstract the complexity of time synchronization
+ * over the network and provide a simple interface for obtaining the current time.
+ */
+
 #ifndef TimeClient_h
 #define TimeClient_h
 
+/**
+ * @brief Initializes the network time protocol (NTP) client.
+ *
+ * Sets up the NTP client with default server and timezone configurations. This function
+ * should be called at the beginning of the program to ensure accurate time is available
+ * for time-stamp or scheduling purposes. It configures the NTP client to synchronize
+ * the device's time with a remote NTP server.
+ */
 void setupTimeClient();
+
+/**
+ * @brief Retrieves the current UNIX timestamp.
+ *
+ * Obtains the current time as a UNIX timestamp (the number of seconds that have elapsed
+ * since January 1, 1970). The time is synchronized over the network using the NTP client
+ * initialized by setupTimeClient().
+ *
+ * @return The current time as an unsigned long representing the UNIX timestamp. This
+ * value can be used to perform time-related operations, such as scheduling tasks or
+ * creating time-stamped records.
+ */
 unsigned long getCurrentTime();
 
-#endif
+#endif // TimeClient_h
 
--- include/WiFiConnection.h
+++ include/WiFiConnection.h
@@ -1,7 +1,24 @@
+/**
+ * @file WiFiConnection.h
+ * @brief Interface for WiFi network connection.
+ *
+ * This header file provides the declaration for the connectToWiFi function,
+ * which is responsible for establishing a connection to a WiFi network using
+ * predefined network credentials.
+ */
+
 #ifndef WiFiConnection_h
 #define WiFiConnection_h
 
+/**
+ * @brief Connects the device to a WiFi network.
+ *
+ * Initiates a connection to a WiFi network using the SSID and password specified
+ * in the project's secrets file. This function will block execution until a connection
+ * is established or the connection attempt fails. It prints the connection status
+ * to the Serial monitor.
+ */
 void connectToWiFi();
 
-#endif
+#endif // WiFiConnection_h
 
