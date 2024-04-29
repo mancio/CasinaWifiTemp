f3bd6b0d25a6335acf1bd9de96db455ba5c318fb
Andrea Mancini <mancioman2@gmail.com>
2024-04-29 13:46:26 +0200

    serial false

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,7 +7,7 @@
 #include "SerialManager.h"
 
 void setup() {
-    initializeSerial(true, 9600);
+    initializeSerial(false, 9600);
     bool wifi = connectToWiFi();
     if (wifi) {
         initializeFirebase();
