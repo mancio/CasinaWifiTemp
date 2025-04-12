2fb62454ae59113fc77d99c5176ac2fe20eaa4d2
Andrea Mancini <mancioman2@gmail.com>
2025-04-12 15:05:24 +0200

    try reconnection less time to avoid battery drain

--- src/WiFiConnection.cpp
+++ src/WiFiConnection.cpp
@@ -3,10 +3,10 @@
 #include "Secrets.h"
 
 bool connectToWiFi() {
-    int attempts = 0;  // Initialize connection attempts counter
+    int attempts = 1;  // Initialize connection attempts counter
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     Serial.print("Connecting to WiFi ..");
-    while (WiFi.status() != WL_CONNECTED && attempts < 4) {  // Check connection status and attempt limit
+    while (WiFi.status() != WL_CONNECTED && attempts <= 2) {  // Check connection status and attempt limit
         Serial.print('.');
         delay(2000);
         attempts++;  // Increment the counter after each attempt
