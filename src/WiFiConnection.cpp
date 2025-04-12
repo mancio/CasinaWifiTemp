197cc529bcc5dcb1044269aacbaf763695f52202
Andrea Mancini <mancioman2@gmail.com>
2025-04-12 16:59:11 +0200

    double wifi configuration and better connection

--- src/WiFiConnection.cpp
+++ src/WiFiConnection.cpp
@@ -1,22 +1,30 @@
 #include <ESP8266WiFi.h>
-#include "WiFiConnection.h"
+#include <ESP8266WiFiMulti.h>
 #include "Secrets.h"
 
+ESP8266WiFiMulti wifiMulti;
+
 bool connectToWiFi() {
-    int attempts = 1;  // Initialize connection attempts counter
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    Serial.print("Connecting to WiFi ..");
-    while (WiFi.status() != WL_CONNECTED && attempts <= 2) {  // Check connection status and attempt limit
-        Serial.print('.');
-        delay(2000);
-        attempts++;  // Increment the counter after each attempt
+    // Add both WiFi networks to WiFiMulti
+    wifiMulti.addAP(WIFI_SSID_1, WIFI_PASSWORD_1);
+    wifiMulti.addAP(WIFI_SSID_2, WIFI_PASSWORD_2);
+
+    Serial.println("Connecting to WiFi...");
+    unsigned long startTime = millis();
+    unsigned long timeout = 10000; // 10 seconds timeout
+
+    // Attempt to connect within the timeout period
+    while (wifiMulti.run() != WL_CONNECTED && millis() - startTime < timeout) {
+        Serial.print(".");
+        delay(500);
     }
+
     if (WiFi.status() == WL_CONNECTED) {
-        Serial.println(" connected");
-        Serial.println(WiFi.localIP());
+        Serial.println("\nConnected to WiFi");
+        Serial.println("IP Address: " + WiFi.localIP().toString());
         return true;
     } else {
-        Serial.println(" connection failed");
+        Serial.println("\nFailed to connect to WiFi");
+        return false;
     }
-    return false;
 }
